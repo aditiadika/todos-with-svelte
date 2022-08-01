7cfb0cc41f996e23aa24da02e9038f79e7cdde72
dika aditia <aditiaaadicka@gmail.com>
2022-08-01 11:15:00 +0700

    setup todos

---
+++ src/App.svelte
@@ -0,0 +1,127 @@
+<script>
+  import "./css/reset.css"
+  import "./css/style.css"
+
+  let todoTitle = "";
+  let todoId = 4;
+  let todos = [
+    {
+      id: 1,
+      title: "Finish Svelte screencast",
+      isComplete: false,
+      isEditing: false,
+    },
+    {
+      id: 2,
+      title: "Go shopping",
+      isComplete: true,
+      isEditing: false,
+    },
+    {
+      id: 3,
+      title: "Take over world",
+      isComplete: false,
+      isEditing: false,
+    },
+  ];
+
+  const addTodo = () => {
+    todos = [
+      ...todos,
+      {
+        id: todoId,
+        title: todoTitle,
+        isComplete: false,
+        isEditing: false,
+      },
+    ]
+
+    todoTitle = ""
+    todoId++
+  }
+
+  const deleteTodo = (id) => {
+    todos = todos.filter(todo => !todo.id).length
+  }
+
+  $: remainingTodos = todos.filter(todo => todo.isComplete == false).length
+
+  const checkAll = () => {
+    todos = todos.map(todo => {
+      todo.isComplete = true
+      return todo
+    });
+  }
+
+  const clearAll = () => {
+    todos = todos.filter(todo => !todo.isComplete)
+  }
+
+</script>
+
+<div class="todo-app-container">
+  <div class="todo-app">
+    <h2>Todo App</h2>
+    <form on:submit|preventDefault="{addTodo}">
+      <input
+        type="text"
+        class="todo-input"
+        placeholder="What do you need to do?"
+        bind:value={todoTitle}
+      />
+    </form>
+    <ul class="todo-list">
+
+      {#each todos as todo, index}
+        <li class="todo-item-container">
+        <div class="todo-item">
+          <input type="checkbox" bind:checked={todo.isComplete}/>
+          <span 
+            class="todo-item-label"
+            class:line-through={todo.isComplete}
+          >{todo.title}</span>
+          <!-- <input
+            type="text"
+            class="todo-item-input"
+            value="Finish Svelte Series"
+          /> -->
+        </div>
+        <button class="x-button" on:click="{deleteTodo(todo.id)}">
+          <svg
+            class="x-button-icon"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </li>
+      {/each}
+    </ul>
+
+    <div class="check-all-container">
+      <div>
+        <button class="button" on:click={checkAll}>Check All</button>
+      </div>
+      <span>{remainingTodos} items remaining</span>
+    </div>
+
+    <div class="other-buttons-container">
+      <div>
+        <button class="button filter-button filter-button-active"> All </button>
+        <button class="button filter-button">Active</button>
+        <button class="button filter-button">Completed</button>
+      </div>
+      <div>
+        <button class="button" on:click="{clearAll}">Clear completed</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  let count = 0;
+  let text = "";
+
+  const increment = () => {
+    count += 1
+  }
+  const decrement = () => {
+    count -= 1
+  }
+</script>
+
+<p>Count is {count}</p>
+{#if count > 5}
+  <p>Count is higher</p>
+{/if}
+
+<button on:click={increment}>Increment</button>
+<button on:click={decrement}>Decrement</button>
